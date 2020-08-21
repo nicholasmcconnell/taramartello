ac787be61142f7cdb7ee78fc69a9d595c69c5da6
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-08-21 18:07:02 -0400

    Subscribe form successfully adds email to clients mailchip subsribers list

--- client/src/components/layout/EmailSignup.js
+++ client/src/components/layout/EmailSignup.js
@@ -11,10 +11,41 @@ export default function EmailSignup() {
 
     return (
         <>
-            <form className='footerform' onSubmit={submit}> <br></br>
+            {/* <div id="mc_embed_signup"> */}
+            <form action="https://taramartello.us17.list-manage.com/subscribe/post?u=388b4c4e898214aaf5d7be920&amp;id=9a1f10895f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate footerform" target="_blank" novalidate>
                 <h2>Sign up for my Newsletter!</h2>
                 <p>(description of newsletter)</p>
-                {/* <label htmlFor='register-email'></label> */}
+                <div id="mc_embed_signup_scroll">
+                    {/* <label htmlFor="mce-EMAIL"></label> */}
+                    <input
+                        type="email"
+                        value={email}
+                        name="EMAIL"
+                        className="email"
+                        id="mce-EMAIL"
+                        placeholder="Enter email"
+                        // required 
+                        onChange={e => setEmail(e.target.value)} />
+
+                    <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+                        <input type="text" name="b_388b4c4e898214aaf5d7be920_9a1f10895f" tabindex="-1" value="" />
+                    </div>
+
+                    {/* <div class="clear"> */}
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                    {/* </div> */}
+                </div>
+            </form>
+            {/* </div> */}
+            {/*  */}
+
+
+
+
+            {/* <form className='footerform' onSubmit={submit}> <br></br>
+                <h2>Sign up for my Newsletter!</h2>
+                <p>(description of newsletter)</p>
+                <label htmlFor='register-email'></label>
                 <input
                     id='register-email'
                     type='email'
@@ -22,7 +53,7 @@ export default function EmailSignup() {
                     onChange={e => setEmail(e.target.value)}
                 />
                 <input type='submit' value='Signup' />
-            </form>
+            </form> */}
         </>
     )
 }
